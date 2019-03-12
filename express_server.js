5f20eb17e3ef839fb212ffa8919e49f3503d6d46
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-12 18:46:30 +0000

    short url and long url commit

--- express_server.js
+++ express_server.js
@@ -9,6 +9,8 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -17,9 +19,15 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+  app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL] }
+    res.render("urls_show", templateVars);
+  });
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
